2783b760e3723d81c85438a5af911d44f7a4f7f7
YamadaTakahiro <takahiro.yamada.vertex@gmail.com>
2018-08-06 14:00:33 +0900

    Initial commit

--- lesson4 ToTwitter/ViewController.swift
+++ lesson4 ToTwitter/ViewController.swift
@@ -1,25 +1,94 @@
-//
-//  ViewController.swift
-//  lesson4 ToTwitter
-//
-//  Created by Yamada Takahiro on 2018/08/06.
-//  Copyright © 2018年 takahiro. All rights reserved.
-//
+//import UIKit
+   import UIKit
+//import Social
+   import Social
 
-import UIKit
 
-class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+
+//    class ViewController: UIViewController {
+       class ViewController: UIViewController {
+    
+//    var myComposeView : SLComposeViewController!
+       var myComposeView : SLComposeViewController!
+//    var myTwitterButton: UIButton!
+       var myTwitterButton: UIButton!
+    
+//      override func viewDidLoad() {
+         override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//     super.viewDidLoad()
+//
+        
+        
+        // Twitter用のUIButtonの生成.
+//        let hex: Int = 0x55ACEE
+            let hex: Int = 0x55ACEE
+//         let red = Double((hex & 0xFF0000) >> 16) / 255.0
+            let red = Double((hex & 0xFF0000) >> 16) / 255.0
+//        let green = Double((hex & 0xFF00) >> 8) / 255.0
+           let green = Double((hex & 0xFF00) >> 8) / 255.0
+//        let blue = Double((hex & 0xFF)) / 255.0
+           let bule = Double((hex & 0xFF)) / 255.0
+            
+            let myColor: UIColor = UIColor( red: CGFloat(red), green: CGFloat(green), blue: CGFloat(bule), alpha: CGFloat(1.0))
+//            let myColor: UIColor = UIColor( red: CGFloat(red), green: CGFloat(green), blue: CGFloat(bule), alpha: CGFloat(1.0))
+//
+        // ボタン.
+//        myTwitterButton = UIButton()
+            myTwitterButton = UIButton()
+//          myTwitterButton.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+             myTwitterButton.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+//        myTwitterButton.backgroundColor = myColor
+           myTwitterButton.backgroundColor = myColor
+//            myTwitterButton.layer.masksToBounds = true
+               myTwitterButton.layer.masksToBounds = true
+//            myTwitterButton.setTitle("Twitter", for: UIControlState.normal)
+            myTwitterButton.setTitle("Twitter", for: UIControlState.normal)
+//         myTwitterButton.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(20))
+            myTwitterButton.titleLabel?.font = UIFont.systemFont(ofSize: CGFloat(20))
+            
+//        myTwitterButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+           myTwitterButton.setTitleColor(UIColor.white, for: UIControlState.normal)
+//        myTwitterButton.layer.cornerRadius = 50.0
+           myTwitterButton.layer.cornerRadius = 50.0
+//        myTwitterButton.layer.position = CGPoint(x: self.view.frame.width/2, y:self.view.frame.height/2)
+            myTwitterButton.layer.position = CGPoint(x: self.view.frame.width/2, y: self.view.frame.height/2)
+//        myTwitterButton.tag = 1
+            myTwitterButton.tag = 1
+//         myTwitterButton.addTarget(self, action: #selector(ViewController.onPostToTwitter(sender:)), for: .touchUpInside)
+            myTwitterButton.addTarget(self, action: #selector(ViewController.onPostToTwitter(sender:)), for: .touchUpInside)
+        
+        // buttonをviewに追加.
+        self.view.addSubview(myTwitterButton)
+            self.view.addSubview(myTwitterButton)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // ボタンイベント.
+//    @objc func onPostToTwitter(sender : AnyObject) {
+        @objc func onPostToTwitter(sender: AnyObject){
+        
+        // SLComposeViewControllerのインスタンス化.
+        // ServiceTypeをTwitterに指定.
+//            myComposeView = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            myComposeView = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+        
+        // 投稿するテキストを指定.
+//        myComposeView.setInitialText("Twitter Test from Swift")
+        myComposeView.setInitialText("Twitter Test from Swift")
+        // 投稿する画像を指定.
+//        myComposeView.add(UIImage(named: "sample.jpg"))
+           myComposeView.add(UIImage(named: "sample.jpg"))
+        
+        // myComposeViewの画面遷移.
+//            self.present(myComposeView, animated: true, completion: nil)
+            self.present(myComposeView, animated: true, completion: nil)
     }
-
-
 }
+
+
+
+
+
+
 
